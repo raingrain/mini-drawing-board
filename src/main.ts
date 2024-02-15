202482ba8ec59137a933bc783ab8e9ed2b3a86fd
raingrain <2711835261@qq.com>
2024-02-15 11:12:42 +0800

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import "./style.scss";
+import "./style.css";
 
 // canvas对象
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 // 设置为2d
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-// 绘制不同的形状+画笔+清除按钮
-const toolBtns = document.querySelectorAll(".tool") as unknown as HTMLLIElement[];
+// 绘制不同的形状 + 画笔 + 清除按钮
+const toolButtons = document.querySelectorAll(".tool") as any as HTMLLIElement[];
 // 是否以填充形式绘制
-const fillColor = document.getElementById("fill-color") as HTMLInputElement;
+const fillColor = document.querySelector("#fill-color") as HTMLInputElement;
 // 线条的尺寸
-const sizeSlider = document.getElementById("size-slider") as HTMLInputElement;
+const sizeSlider = document.querySelector("#size-slider") as HTMLInputElement;
 // 颜色选择按钮
-const colorBtns = document.querySelectorAll(".colors .option") as unknown as HTMLLIElement[];
+const colorButtons = document.querySelectorAll(".colors .option") as any as HTMLLIElement[];
 // 颜色选择器按钮
-const colorPicker = document.getElementById("color-picker") as HTMLInputElement;
+const colorPicker = document.querySelector("#color-picker") as HTMLInputElement;
 // 清除画布按钮
 const clearCanvas = document.querySelector(".clear-canvas") as HTMLButtonElement;
 // 保存为图像按钮
@@ -25,72 +25,20 @@ let prevMouseX: number, prevMouseY: number;
 let snapshot: ImageData;
 // 是否开启绘制
 let isDrawing = false;
-// 笔刷/线条宽度
-let brushWidth = 5;
 // 默认选择的工具
 let selectedTool = "brush";
+// 笔刷/线条宽度
+let brushWidth = 5;
 // 选择的颜色
 let selectedColor = "#000";
 
-// 绘制时线宽度由range输入框决定
-sizeSlider.addEventListener("change", function changeBrushWidth() {
-    // brushWidth = (e.target as HTMLInputElement).value;
-    brushWidth = Number(this.value);
-});
-
-// 工具选中时赋予样式并清除之前的
-toolBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        (document.querySelector(".options .active") as HTMLLIElement).classList.remove("active");
-        btn.classList.add("active");
-        selectedTool = btn.id;
-    });
-});
-
-// 切换选择的颜色
-colorBtns.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        // 清除选择样式
-        document.querySelector(".options .selected")!.classList.remove("selected");
-        // 给当前按钮添加样式
-        (e.target as HTMLLIElement).classList.add("selected");
-        // 从window上获取选中对象的颜色
-        selectedColor = (e.target as HTMLLIElement).style.backgroundColor;
-    });
-});
-
-// 针对于最后一个颜色选择器的函数
-colorPicker.addEventListener("change", (e) => {
-    // 当选择器的颜色被更改时，把包含它的li的颜色改了，然后手动点击li
-    colorPicker.parentElement!.style.backgroundColor = (<HTMLInputElement>e.target).value;
-    colorPicker.parentElement!.click();
-});
-
-// 清除画布
-clearCanvas.addEventListener("click", function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    setCanvasBackgroundColor();
-});
-
-// 保存图片，直接利用a标签的下载属性来进行
-saveImg.addEventListener("click", () => {
-    // 先创建a
-    const link = document.createElement("a");
-    // 以日期来命名下载的文件
-    link.download = `${Date.now()}.jpg`;
-    // 下载链接就是图片的地址
-    link.href = canvas.toDataURL();
-    // 点击下载
-    link.click();
-});
-
 // 给一张空画布添加一张白色背景
-function setCanvasBackgroundColor() {
+const setCanvasBackground = () => {
     ctx.fillStyle = "#fff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     // 添加后记得把画笔的颜色改成之前选中的
     ctx.fillStyle = selectedColor;
-}
+};
 
 window.addEventListener("load", () => {
     // 把canvas的宽高从默认(300, 150)修改为容器宽高
@@ -98,19 +46,45 @@ window.addEventListener("load", () => {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
     // 设置一个白色背景
-    setCanvasBackgroundColor();
+    setCanvasBackground();
 });
 
+// 绘制矩形
+const drawRect = (e: MouseEvent) => {
+    // 根据填充按钮是否被选中来控制绘制描边矩形还是填充矩形
+    if (!fillColor.checked) {
+        return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+    }
+    ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+};
 
-// 鼠标按下移动抬起的一个过程就是绘制过程
-canvas.addEventListener("mousedown", startDraw);
-canvas.addEventListener("mousemove", drawing);
-canvas.addEventListener("mouseup", endDraw);
+// 绘制圆形
+const drawCircle = (e: MouseEvent) => {
+    ctx.beginPath();
+    // 鼠标初始位置与当前位置之间的直线距离作为半径
+    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
+    // 根据填充按钮是否被选中来控制绘制描边圆形还是填充圆形
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+};
 
-// 绘制相关涉及到的函数
+// 三角形绘制函数
+const drawTriangle = (e: MouseEvent) => {
+    ctx.beginPath();
+    // 绘制顶点
+    ctx.moveTo(prevMouseX, prevMouseY);
+    // 顶点到底边一个点
+    ctx.lineTo(e.offsetX, e.offsetY);
+    // 绘制等腰三角形，获取底边的另外一个对称点
+    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+    // 三角形需要闭合，也可以使用lineTo回到原点
+    ctx.closePath();
+    // 看看是否需要填充
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+};
 
 // 开始绘制函数
-function startDraw(e: MouseEvent) {
+const startDraw = (e: MouseEvent) => {
     isDrawing = true;
     // 获取鼠标初始位置
     prevMouseX = e.offsetX;
@@ -122,63 +96,86 @@ function startDraw(e: MouseEvent) {
     ctx.fillStyle = selectedColor;
     // 将之前的绘制数据拷贝并存入到一个快照变量中
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
-}
+};
 
 // 绘制中函数
-function drawing(e: MouseEvent) {
+const drawing = (e: MouseEvent) => {
     // 如果不在绘制状态就返回
-    if (!isDrawing) {
-        return;
-    }
+    if (!isDrawing) return;
     // 将快照（之前的绘制数据）打印在画布上
     ctx.putImageData(snapshot, 0, 0);
     // 根据选择进行绘制
     if (selectedTool === "brush" || selectedTool === "eraser") {
         // 选择橡皮擦就把线条颜色改成白色即可
         ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
+        ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse pointer
+        ctx.stroke(); // drawing/filling line with color
     } else if (selectedTool === "rectangle") {
         drawRect(e);
     } else if (selectedTool === "circle") {
         drawCircle(e);
-    } else if (selectedTool === "triangle") {
+    } else {
         drawTriangle(e);
     }
-}
+};
 
-// 矩形绘制函数
-function drawRect(e: MouseEvent) {
-    ctx.beginPath();
-    // 根据填充按钮是否被选中来控制绘制描边矩形还是填充矩形
-    fillColor.checked
-        ? ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY)
-        : ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
-}
-
-// 圆形绘制函数
-function drawCircle(e: MouseEvent) {
-    ctx.beginPath();
-    // 鼠标初始位置与当前位置之间的直线距离作为半径
-    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
-    ctx.arc(prevMouseX, prevMouseY, radius, 0 / 180, 360 * Math.PI / 180);
-    // 根据填充按钮是否被选中来控制绘制描边圆形还是填充圆形
-    fillColor.checked ? ctx.fill() : ctx.stroke();
-}
-
-// 三角形绘制函数
-function drawTriangle(e: MouseEvent) {
-    ctx.beginPath();
-    ctx.moveTo(prevMouseX, prevMouseY);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    // 绘制等腰三角形，获取底边的另外一个对称点
-    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
-    // 三角形需要闭合，也可以使用lineTo回到原点
-    ctx.closePath();
-    fillColor.checked ? ctx.fill() : ctx.stroke();
-}
-
-// 结束绘制函数
+// 结束绘制
 function endDraw() {
     isDrawing = false;
 }
+
+// 工具选中时赋予样式并清除之前的
+toolButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector(".options .active")?.classList.remove("active");
+        btn.classList.add("active");
+        selectedTool = btn.id;
+    });
+});
+
+// 绘制时线宽度由range输入框决定
+sizeSlider.addEventListener("change", () => brushWidth = Number(sizeSlider.value));
+
+// 切换选择的颜色
+colorButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        // 清除选择样式
+        document.querySelector(".options .selected")?.classList.remove("selected");
+        // 给当前按钮添加样式
+        btn.classList.add("selected");
+        // 从window上获取选中对象的颜色
+        selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+    });
+});
+
+// 针对于最后一个颜色选择器的函数
+colorPicker.addEventListener("change", () => {
+    colorPicker.parentElement!.style.background = colorPicker.value;
+    colorPicker.parentElement?.click();
+});
+
+// 清除画布
+clearCanvas.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    setCanvasBackground();
+});
+
+// 保存图片，直接利用a标签的下载属性来进行
+saveImg.addEventListener("click", () => {
+    // 先创建a标签
+    const link = document.createElement("a");
+    // 以日期来命名下载的文件
+    link.download = `${Date.now()}.jpg`;
+    // 下载链接就是图片的地址
+    link.href = canvas.toDataURL();
+    // 点击下载
+    link.click();
+});
+
+// 鼠标按下移动抬起的一个过程就是绘制过程
+// 开始绘制
+canvas.addEventListener("mousedown", startDraw);
+// 绘制中
+canvas.addEventListener("mousemove", drawing);
+// 结束绘制函数
+canvas.addEventListener("mouseup", endDraw);
